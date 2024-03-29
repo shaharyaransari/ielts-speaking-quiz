faadbd765985c572a0ec4b40d69001401477c3a6
shaharyar <shaharyaransari2783912@gmail.com>
2024-03-29 07:24:03 +0500

    Code Cleanup

--- Inc/External/PronunciationAPI.php
+++ Inc/External/PronunciationAPI.php
@@ -9,7 +9,6 @@ class PronunciationAPI{
     public static function get_pronunciation_data($audioUrl,$transcript){
         // $audioUrl = 'http://wordpress4all.com/wp-content/uploads/2024/03/wrong-pronun-1.mp3';
         // $transcript = "I seen her at the store yesterday. Me and her talked about the upcoming party. She don't know what to wear yet, but I told her to not worry.";
-        // $speech_key = 'ca8fc23e971f4f74915c0c627e7a5283';
         $speech_key = self::$speech_key;
         if(!$speech_key){
             return null;
